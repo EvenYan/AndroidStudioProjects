7f611e9bc7813cda3e3072a59bbe3e3e12f313e7
EvenYan <evenyan@EvenYans-MacBook-Pro-2.local>
2016-07-20 14:55:12 +0800

    BusinessCard-V 0.1(Layout without function)

---
+++ BusinessCard/app/src/main/java/com/example/evenyan/businesscard/CardFragment.java
@@ -0,0 +1,139 @@
+package com.example.evenyan.businesscard;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 我想知道的碎片页面
+ * @author wwj_748
+ *
+ */
+public class CardFragment extends Fragment {
+
+    public static final int TAKE_PHOTO = 1;
+
+    public static final int CROP_PHOTO = 2;
+
+    private ImageView iv;
+
+    private Uri imageUri;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_card, container,
+                false);
+
+        //设置相机监听
+        iv = (ImageView) view.findViewById(R.id.iv_camera);
+        Log.d("CardFragment", "Null");
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 创建File对象,用于存储拍照后的图片
+                File outputImage = new File(Environment.
+                        getExternalStorageDirectory(), "tempImage.jpg");
+                try {
+                    if (outputImage.exists()) {
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageUri = Uri.fromFile(outputImage);
+                Intent intent = new Intent("android.media.action. IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO); // 启动相机程序
+
+            }
+        });
+
+        return view;
+    }
+
+    /**
+     * A simple {@link Fragment} subclass.
+     * Activities that contain this fragment must implement the
+     * {@link HomeFragment.OnFragmentInteractionListener} interface
+     * to handle interaction events.
+     */
+    public static class HomeFragment extends Fragment {
+
+        private OnFragmentInteractionListener mListener;
+
+        public HomeFragment() {
+            // Required empty public constructor
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_home, container, false);
+        }
+
+        // TODO: Rename method, update argument and hook method into UI event
+        public void onButtonPressed(Uri uri) {
+            if (mListener != null) {
+                mListener.onFragmentInteraction(uri);
+            }
+        }
+
+        @Override
+        public void onAttach(Context context) {
+            super.onAttach(context);
+            if (context instanceof OnFragmentInteractionListener) {
+                mListener = (OnFragmentInteractionListener) context;
+            } else {
+                throw new RuntimeException(context.toString()
+                        + " must implement OnFragmentInteractionListener");
+            }
+        }
+
+        @Override
+        public void onDetach() {
+            super.onDetach();
+            mListener = null;
+        }
+
+        /**
+         * This interface must be implemented by activities that contain this
+         * fragment to allow an interaction in this fragment to be communicated
+         * to the activity and potentially other fragments contained in that
+         * activity.
+         * <p/>
+         * See the Android Training lesson <a href=
+         * "http://developer.android.com/training/basics/fragments/communicating.html"
+         * >Communicating with Other Fragments</a> for more information.
+         */
+        public interface OnFragmentInteractionListener {
+            // TODO: Update argument type and name
+            void onFragmentInteraction(Uri uri);
+        }
+    }
+}
---
+++ BusinessCard/app/src/main/java/com/example/evenyan/businesscard/MainActivity.java
@@ -0,0 +1,200 @@
+//package com.example.evenyan.businesscard;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+
+package com.example.evenyan.businesscard;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * 主Activity
+ *
+ * @author wwj_748
+ *
+ */
+public class MainActivity extends FragmentActivity implements OnClickListener {
+
+    // 三个tab布局
+    private RelativeLayout homeLayout, cardLayout, meLayout;
+
+    // 底部标签切换的Fragment
+    private Fragment homeFragment, cardFragment, meFragment, currentFragment;
+    // 底部标签图片
+    private ImageView homeImg, cardImg, meImg;
+    // 底部标签的文本
+    private TextView homeTv, cardTv, meTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initUI();
+        initTab();
+    }
+
+    /**
+     * 初始化UI
+     */
+    private void initUI() {
+        homeLayout = (RelativeLayout) findViewById(R.id.rl_know);
+        cardLayout = (RelativeLayout) findViewById(R.id.rl_want_know);
+        meLayout = (RelativeLayout) findViewById(R.id.rl_me);
+        homeLayout.setOnClickListener(this);
+        cardLayout.setOnClickListener(this);
+        meLayout.setOnClickListener(this);
+
+        homeImg = (ImageView) findViewById(R.id.iv_know);
+        cardImg = (ImageView) findViewById(R.id.iv_i_want_know);
+        meImg = (ImageView) findViewById(R.id.iv_me);
+        homeTv = (TextView) findViewById(R.id.tv_know);
+        cardTv = (TextView) findViewById(R.id.tv_i_want_know);
+        meTv = (TextView) findViewById(R.id.tv_me);
+
+    }
+
+    /**
+     * 初始化底部标签
+     */
+    private void initTab() {
+        if (homeFragment == null) {
+            homeFragment = new HomeFragment();
+        }
+
+        if (!homeFragment.isAdded()) {
+            // 提交事务
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.content_layout, homeFragment).commit();
+
+            // 记录当前Fragment
+            currentFragment = homeFragment;
+            // 设置图片文本的变化
+            homeImg.setImageResource(R.drawable.home_pre);
+            homeTv.setTextColor(getResources()
+                    .getColor(R.color.bottomtab_press));
+            cardImg.setImageResource(R.drawable.card_nor);
+            cardTv.setTextColor(getResources().getColor(
+                    R.color.bottomtab_normal));
+            meImg.setImageResource(R.drawable.me_nor);
+            meTv.setTextColor(getResources().getColor(R.color.bottomtab_normal));
+
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.rl_know: // 知道
+                clickTab1Layout();
+                break;
+            case R.id.rl_want_know: // 我想知道
+                clickTab2Layout();
+                break;
+            case R.id.rl_me: // 我的
+                clickTab3Layout();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 点击第一个tab
+     */
+    private void clickTab1Layout() {
+        if (homeFragment == null) {
+            homeFragment = new HomeFragment();
+        }
+        addOrShowFragment(getSupportFragmentManager().beginTransaction(), homeFragment);
+
+        // 设置底部tab变化
+        homeImg.setImageResource(R.drawable.home_pre);
+        homeTv.setTextColor(getResources().getColor(R.color.bottomtab_press));
+        cardImg.setImageResource(R.drawable.card_nor);
+        cardTv.setTextColor(getResources().getColor(
+                R.color.bottomtab_normal));
+        meImg.setImageResource(R.drawable.me_nor);
+        meTv.setTextColor(getResources().getColor(R.color.bottomtab_normal));
+    }
+
+    /**
+     * 点击第二个tab
+     */
+    private void clickTab2Layout() {
+        if (cardFragment == null) {
+            cardFragment = new CardFragment();
+        }
+        addOrShowFragment(getSupportFragmentManager().beginTransaction(), cardFragment);
+
+        homeImg.setImageResource(R.drawable.home_nor);
+        homeTv.setTextColor(getResources().getColor(R.color.bottomtab_normal));
+        cardImg.setImageResource(R.drawable.card_pre);
+        cardTv.setTextColor(getResources().getColor(
+                R.color.bottomtab_press));
+        meImg.setImageResource(R.drawable.me_nor);
+        meTv.setTextColor(getResources().getColor(R.color.bottomtab_normal));
+
+    }
+
+    /**
+     * 点击第三个tab
+     */
+    private void clickTab3Layout() {
+        if (meFragment == null) {
+            meFragment = new MeFragment();
+        }
+
+        addOrShowFragment(getSupportFragmentManager().beginTransaction(), meFragment);
+        homeImg.setImageResource(R.drawable.home_nor);
+        homeTv.setTextColor(getResources().getColor(R.color.bottomtab_normal));
+        cardImg.setImageResource(R.drawable.card_nor);
+        cardTv.setTextColor(getResources().getColor(
+                R.color.bottomtab_normal));
+        meImg.setImageResource(R.drawable.me_pre);
+        meTv.setTextColor(getResources().getColor(R.color.bottomtab_press));
+
+    }
+
+    /**
+     * 添加或者显示碎片
+     *
+     * @param transaction
+     * @param fragment
+     */
+    private void addOrShowFragment(FragmentTransaction transaction,
+                                   Fragment fragment) {
+        if (currentFragment == fragment)
+            return;
+
+        if (!fragment.isAdded()) { // 如果当前fragment未被添加，则添加到Fragment管理器中
+            transaction.hide(currentFragment)
+                    .add(R.id.content_layout, fragment).commit();
+        } else {
+            transaction.hide(currentFragment).show(fragment).commit();
+        }
+
+        currentFragment = fragment;
+    }
+
+}
