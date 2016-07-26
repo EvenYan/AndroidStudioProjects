9a399aa73a8ed339849932c01843ff9b5d1c80b0
evenyan <1014934663@qq.com>
2016-07-27 07:22:16 +0800

    Try to fix HTTP/POST

--- BusinessCard/app/src/main/java/com/example/evenyan/businesscard/CardActivityFragment.java
+++ BusinessCard/app/src/main/java/com/example/evenyan/businesscard/CardActivityFragment.java
@@ -4,23 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import okhttp3.MediaType;
 
 /**
  * 我想知道的碎片页面
@@ -35,6 +26,8 @@ public class CardActivityFragment extends Fragment {
     private Uri fileUri;
     private ImageView picture;
     private ContactDatebase dbHelper;
+    public static final String TAG = "MainActivity";
+    public static final MediaType JSON=MediaType.parse("application/json; charset=utf-8");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,8 +36,18 @@ public class CardActivityFragment extends Fragment {
 
     }
 
+    //        Toast.makeText(this, url, Toast.LENGTH_LONG).show();
+//    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+//    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+//    startActivityForResult(intent, TAKE_PHOTO); // 启动相机程序
 
-
+//        try {
+//            String bs = imgToBase64(uri, BitmapFactory.decodeStream(getContentResolver().openInputStream(fileUri)), "jpg");
+//            Toast.makeText(this, bs.substring(0, 20), Toast.LENGTH_LONG).show();
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -55,132 +58,30 @@ public class CardActivityFragment extends Fragment {
         iv = (ImageView)view.findViewById(R.id.iv_camera);
 //        picture = (ImageView)view.findViewById(R.id.picture);
 
-        dbHelper = new ContactDatebase(getActivity(), "Contact.db", null, 1);
-        Button createDatabase = (Button) view.findViewById(R.id.create_database);
-        createDatabase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dbHelper.getWritableDatabase();
-            }
-        });
+//        dbHelper = new ContactDatebase(getActivity(), "Contact.db", null, 1);
+//        Button createDatabase = (Button) view.findViewById(R.id.create_database);
+//        createDatabase.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dbHelper.getWritableDatabase();
+//            }
+//        });
 
         iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getActivity(), "YOU are me", Toast.LENGTH_SHORT).show();
-                // 创建File对象,用于存储拍照后的图片
-                File outputImage = new File(Environment.
-                        getExternalStorageDirectory(), "tempImage.jpg");
-                try {
-                    if (outputImage.exists()) {
-                        outputImage.delete();
-                    }
-
-                    outputImage.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                fileUri = Uri.fromFile(outputImage);
-
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-                startActivityForResult(intent, TAKE_PHOTO); // 启动相机程序
-
-                String url = "http://bcr2.intsig.net/BCRService/BCR_Crop?user=568036792@qq.com&pass=MC7YFBE4XCQJ45HA&lang=524287";
-                postUseUrlConnection(url, outputImage);
+                Intent intent = new Intent(getActivity(), CardContent.class);
+                startActivity(intent);
             }
         });
+
 
 
         return view;
     }
 
-    public static String postUseUrlConnection(String requestUrl, File file) {
-        String end = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        String newName = "image.jpg";
-        StringBuffer sb = new StringBuffer();
-        try {
-            URL url = new URL(requestUrl);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            // 允许Input、Output，不使用Cache
-            con.setDoInput(true);
-            con.setDoOutput(true);
-            con.setUseCaches(false);
-            // 设置以POST方式进行传送
-            con.setRequestMethod("POST");
-            // 设置RequestProperty
-            con.setRequestProperty("Connection", "Keep-Alive");
-            con.setRequestProperty("Charset", "UTF-8");
-            con.setRequestProperty("Content-Type",
-                    "multipart/form-data;boundary=" + boundary);
-            // 构造DataOutputStream流
-            DataOutputStream ds = new DataOutputStream(con.getOutputStream());
-            ds.writeBytes(twoHyphens + boundary + end);
-            ds.writeBytes("Content-Disposition: form-data; "
-                    + "name=\"file1\";filename=\"" + newName + "\"" + end);
-            ds.writeBytes(end);
-            // 构造要上传文件的FileInputStream流
-            FileInputStream fis = new FileInputStream(file);
-            // 设置每次写入1024bytes
-            int bufferSize = 1024;
-            byte[] buffer = new byte[bufferSize];
-            int length = -1;
-            // 从文件读取数据至缓冲区
-            while ((length = fis.read(buffer)) != -1) {
-                // 将资料写入DataOutputStream中
-                ds.write(buffer, 0, length);
-            }
-            ds.writeBytes(end);
-            ds.writeBytes(twoHyphens + boundary + twoHyphens + end);
-            // 关闭流
-            fis.close();
-            ds.flush();
-            // 获取响应流
-            InputStream is = con.getInputStream();
-            int ch;
-            while ((ch = is.read()) != -1) {
-                sb.append((char) ch);
-            }
-            // 关闭DataOutputStream
-            ds.close();
-            return sb.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
 
-
-//    @Override
-//    public void onActivityResult(int resutCode, int resultCode, Intent data) {
-//
-//        switch (resultCode) {
-//            case TAKE_PHOTO:
-//                if (resultCode == getActivity().RESULT_OK) {
-//                    Intent intent = new Intent("com.android.camera.action.CROP");
-//                    intent.setDataAndType(fileUri, "image/*");
-//                    intent.putExtra("scale", true);
-//                    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-//                    startActivityForResult(intent, CROP_PHOTO);
-//                }
-//                break;
-//            case CROP_PHOTO:
-//                if(resultCode == getActivity().RESULT_OK) {
-//                    try {
-//                        Bitmap bitmap =  BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(fileUri));
-//                        picture.setImageBitmap(bitmap);
-//                    } catch (FileNotFoundException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//                break;
-//            default:
-//                break;
-//
-//        }
-//    }
 
     /**
      * A simple {@link Fragment} subclass.
---
+++ BusinessCard/app/src/main/java/com/example/evenyan/businesscard/CardContent.java
@@ -0,0 +1,238 @@
+package com.example.evenyan.businesscard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
+import okhttp3.MediaType;
+
+public class CardContent extends Activity {
+
+    public static final int TAKE_PHOTO = 1;
+    public static final int SEND_PICTURE = 2;
+    private ImageView iv;
+    private Uri fileUri;
+    private ImageView picture;
+    private ContactDatebase dbHelper;
+    public static final String TAG = "MainActivity";
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    String uri;
+
+    public static final int REQUEST_CAPTURE_IMAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_card_content);
+//        picture = (ImageView)findViewById(R.id.picture);
+
+        // 创建File对象,用于存储拍照后的图片
+        File outputImage = new File(Environment.
+                getExternalStorageDirectory(), createFileName());
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+
+            outputImage.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        fileUri = Uri.fromFile(outputImage);
+        uri = toString().valueOf(fileUri);
+
+        uri=uri.replace("file:/","");
+//        Toast.makeText(this, url, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(intent, TAKE_PHOTO); // 启动相机程序
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(fileUri));
+            String bs64 =bitmaptoString(bitmap);
+//            Toast.makeText(this, bs64.substring(0, 20), Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+//        try {
+//            Toast.makeText(this, imgToBase64(uri, BitmapFactory.decodeStream(getContentResolver().openInputStream(fileUri)), "jpg").substring(0, 20), Toast.LENGTH_LONG).show();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+
+        //开启一个线程,做联网操作
+//        new Thread(){
+//            @Override
+//            public void run(){
+//                OkHttpClient client = new OkHttpClient();
+//                String post(String url, String json) throws IOException{
+//                    RequestBody body = new RequestBody.create(JSON, json);
+//
+//                }
+//            }
+//        }.start();
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            Toast.makeText(this, "heoll", Toast.LENGTH_SHORT).show();
+
+        }
+
+//        if(requestCode == getActivity().RESULT_OK){
+//            if(requestCode == TAKE_PHOTO) {
+//                Bundle extras = data.getExtras();
+//                Bitmap photoBit = (Bitmap) extras.get("data");
+//            }
+//        }
+//        else {
+//            Toast.makeText(getContext(), "获取资源失败", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+
+//        switch (resultCode) {
+//            case REQUEST_CAPTURE_IMAGE:
+//                if (resultCode == RESULT_OK) {
+//                    Intent intent = new Intent("com.android.camera.action.CROP");
+//                    intent.setDataAndType(fileUri, "image/*");
+//                    intent.putExtra("scale", true);
+//                    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+//                    startActivityForResult(intent, SEND_PICTURE);
+//
+//                }
+//                break;
+//            case SEND_PICTURE:
+//                if (resultCode == RESULT_OK) {
+//                    try {
+//                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(fileUri));
+//                        picture.setImageBitmap(bitmap);
+//                    } catch (FileNotFoundException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                }
+//        file = new File(String.valueOf(fileUri));
+//
+//        FileInputStream inputFile = new FileInputStream(file);
+//        byte[] buffer = new byte[(int)file.length()];
+//        inputFile.read(buffer);
+//        inputFile.close();
+//        String bs64 = Base64.encodeToString(buffer, Base64.DEFAULT);
+//                break;
+//            default:
+//                break;
+//
+//        }
+    }
+
+    public static String createFileName() {
+        String filename = "";
+        Date date = new Date((System.currentTimeMillis()));
+        java.text.SimpleDateFormat dateFormat = new java.text.SimpleDateFormat("'IMG'_yyyyMMdd_HHmmss");
+        filename = dateFormat.format(date) + ".jpg";
+        return filename;
+    }
+
+    //    public static void savePhotoToSDCard(String path, String photoName, Bitmap photoBitMap){
+//        if (android.os.Environment.getExternalStorageDirectory().equals(
+//                Environment.MEDIA_MOUNTED)){
+//            File dir = new File(path);
+//            if (!dir.exists()) {
+//                dir.mkdir();
+//            }
+//            File photoFile = new File(path, photoName);
+//            FileOutputStream fileOutputStream = null;
+//            try {
+//                fileOutputStream = new FileOutputStream(photoFile);
+//                if (photoBitMap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)) {
+//                    fileOutputStream.flush();
+//                    fileOutputStream.close();
+//                };
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                try {
+//                    fileOutputStream.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+
+//    public static String imgToBase64(String imgPath, Bitmap bitmap, String imgFormat) {
+////        String imgPath = String.valueOf(imgUri);
+//        if (imgPath !=null && imgPath.length() > 0) {
+//            bitmap = readBitmap(imgPath);
+//        }
+//        if(bitmap == null){
+//            //bitmap not found!!
+//        }
+//        ByteArrayOutputStream out = null;
+//        try {
+//            out = new ByteArrayOutputStream();
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+//
+//            out.flush();
+//            out.close();
+//
+//            byte[] imgBytes = out.toByteArray();
+//            return Base64.encodeToString(imgBytes, Base64.DEFAULT);
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//            return null;
+//        } finally {
+//            try {
+//                out.flush();
+//                out.close();
+//            } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    private static Bitmap readBitmap(String imgPath) {
+//        try {
+//            return BitmapFactory.decodeFile(imgPath);
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//            return null;
+//        }
+//
+//    }
+
+    public String bitmaptoString(Bitmap bitmap) {
+
+        // 将Bitmap转换成字符串
+        String string = null;
+        ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bStream);
+        byte[] bytes = bStream.toByteArray();
+        string = Base64.encodeToString(bytes, Base64.DEFAULT);
+        return string;
+    }
+
+}
